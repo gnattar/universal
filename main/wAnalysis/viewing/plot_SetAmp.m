dca6cf2b7098b2d5323f11ef0c06c72865d32e14
Gayathri <gnattar@gmail.com>
2013-11-14 10:50:53 -0500

    wAmpDevEpoch

--- main/wAnalysis/viewing/plot_SetAmp.m
+++ main/wAnalysis/viewing/plot_SetAmp.m
@@ -1,7 +1,7 @@
 %% add trialtype and trialnum to wsArray and wSigTrials
 %% 
 
-function plot_SetAmp(d,wsArray,solo_data,restrictTime)
+function plot_SetAmp(d,wsArray,solo_data,restrictTime,mad_threshold,redo)
 
 if(nargin < 2)
 [filename1,path]= uigetfile('wsArray*.mat', 'Load wsArray.mat file');
@@ -11,30 +11,30 @@ load([path filesep filename2]);
 end
 names=cellfun(@(x) x.trackerFileName(length(x.trackerFileName)-21:length(x.trackerFileName)-18),wsArray.ws_trials,'uniformoutput',false);
 wSig_trialnums =str2num(char(names));
+if redo
+    for i = 1:wsArray.nTrials
+        wsArray.ws_trials{i}.trialNum = wSig_trialnums(i);
+        n=wSig_trialnums(i);
+            switch solo_data.trialTypes(n)
+                case 1
+                    if solo_data.trialCorrects(n)
+                         wsArray.ws_trials{i}.trialType = 1; %Hit
+                    else
+                        wsArray.ws_trials{i}.trialType = 2; %Miss
+                    end
+                case 0            
+                    if solo_data.trialCorrects(n)
+                         wsArray.ws_trials{i}.trialType = 3; %CR
+                    else
+                        wsArray.ws_trials{i}.trialType = 4; %FA
+                    end
+            end
+    end
 
-for i = 1:wsArray.nTrials
-    wsArray.ws_trials{i}.trialNum = wSig_trialnums(i);
-    n=wSig_trialnums(i);
-        switch solo_data.trialTypes(n)
-            case 1
-                if solo_data.trialCorrects(n)
-                     wsArray.ws_trials{i}.trialType = 1; %Hit
-                else
-                    wsArray.ws_trials{i}.trialType = 2; %Miss
-                end
-            case 0            
-                if solo_data.trialCorrects(n)
-                     wsArray.ws_trials{i}.trialType = 3; %CR
-                else
-                    wsArray.ws_trials{i}.trialType = 4; %FA
-                end
-        end
+    list = dir('wsArray*.mat');
+    filename1 = list.name;
+    save(filename1,'wsArray');
 end
-
-list = dir('wsArray*.mat');
-filename1 = list.name;
-save(filename1,'wsArray');
-
 %% plot CR trials whisker data
 cd (d);
 if(exist('wsArray'))
@@ -42,8 +42,7 @@ else
     [filename1,path]= uigetfile('wsArray*.mat', 'Load wsArray.mat file');
     load([path filesep filename1]);    
 end
-name = strrep(filename1,'wsArray_','');
-name = strrep(name,'.mat','');
+name = wsArray.sessionName';
 mouseName = wsArray.mouseName;
 name = [mouseName ' ' name];
 wSigTrials = wsArray.ws_trials;
@@ -59,8 +58,12 @@ sc = get(0,'ScreenSize');
 h1b = figure('position', [1000, sc(4)/10-100, sc(3)/2, sc(4)], 'color','w');
 suptitle([name 'CR 11:30']);
 for i = 11:30
-    [tuenv, tlenv]  =  envelope(theta{i});
-    subplot(10,2,i-10);plot(t{i},Set{i},'linewidth',1,'color','b'); hold on;plot(t{i},Amp{i},'linewidth',1, 'color','r');hold on;plot(t{i},tuenv,'linewidth',1, 'color','k');axis([restrictTime(1) restrictTime(2) -30 30]);
+    [tuenv, tlenv]  =  envelope(theta{i}); devepoch = find(Amp{i}>(mad_threshold*mad(Amp{i})));
+    subplot(10,2,i-10);
+    plot(t{i},Set{i},'linewidth',1,'color','b'); hold on; 
+    plot(t{i},Amp{i},'linewidth',1, 'color','r');hold on; 
+    plot(t{i},tuenv,'linewidth',1, 'color','k');hold on; plot(t{i}(devepoch),tuenv(devepoch),'k.','MarkerSize',6); 
+    axis([restrictTime(1) restrictTime(2) -30 30]);
     text(2.25,10,['T' num2str(CRtrials(i))]);
 end
 set(gcf,'PaperPositionMode','auto');
@@ -71,8 +74,8 @@ close(h1b);
 h1b = figure('position', [1000, sc(4)/10-100, sc(3)/2, sc(4)], 'color','w');
 suptitle([name 'CR 31:50']);
 for i = 31:50
-    [tuenv, tlenv]  =  envelope(theta{i});
-    subplot(10,2,i-30);plot(t{i},Set{i},'linewidth',1,'color','b'); hold on;plot(t{i},Amp{i},'linewidth',1, 'color','r');hold on;plot(t{i},tuenv,'linewidth',1, 'color','k');axis([restrictTime(1) restrictTime(2)  -30 30]);
+    [tuenv, tlenv]  =  envelope(theta{i});    devepoch = find(Amp{i}>(mad_threshold*mad(Amp{i})));
+    subplot(10,2,i-30);plot(t{i},Set{i},'linewidth',1,'color','b'); hold on;plot(t{i},Amp{i},'linewidth',1, 'color','r');hold on;plot(t{i},tuenv,'linewidth',1, 'color','k');hold on; plot(t{i}(devepoch),tuenv(devepoch),'k.','MarkerSize',6); hold on;axis([restrictTime(1) restrictTime(2)  -30 30]);
     text(2.25,10,['T' num2str(CRtrials(i))]);
 end
 set(gcf,'PaperPositionMode','auto');
@@ -84,8 +87,8 @@ if(length(theta) > i)
     h1b = figure('position', [1000, sc(4)/10-100, sc(3)/2, sc(4)], 'color','w');
     suptitle([name 'CR 51:70']); 
     for i = 51:min(length(theta),70)
-        [tuenv, tlenv]  =  envelope(theta{i});
-        subplot(10,2,i-50);plot(t{i},Set{i},'linewidth',1,'color','b'); hold on;plot(t{i},Amp{i},'linewidth',1, 'color','r');hold on;plot(t{i},tuenv,'linewidth',1, 'color','k');axis([restrictTime(1) restrictTime(2)  -30 30]);
+        [tuenv, tlenv]  =  envelope(theta{i});  devepoch = find(Amp{i}>(mad_threshold*mad(Amp{i})));
+        subplot(10,2,i-50);plot(t{i},Set{i},'linewidth',1,'color','b'); hold on;plot(t{i},Amp{i},'linewidth',1, 'color','r');hold on;plot(t{i},tuenv,'linewidth',1, 'color','k');hold on; plot(t{i}(devepoch),tuenv(devepoch),'k.','MarkerSize',6);axis([restrictTime(1) restrictTime(2)  -30 30]);
         text(2.25,10,['T' num2str(CRtrials(i))]);
     end
     set(gcf,'PaperPositionMode','auto');
@@ -99,7 +102,7 @@ if(length(theta) > i)
     suptitle([name 'CR 71:90']); 
     for i = 71:min(length(theta),90)
         [tuenv, tlenv]  =  envelope(theta{i});
-        subplot(10,2,i-70);plot(t{i},Set{i},'linewidth',1,'color','b'); hold on;plot(t{i},Amp{i},'linewidth',1, 'color','r');hold on;plot(t{i},tuenv,'linewidth',1, 'color','k');axis([restrictTime(1) restrictTime(2)  -30 30]);
+        subplot(10,2,i-70);plot(t{i},Set{i},'linewidth',1,'color','b'); hold on;plot(t{i},Amp{i},'linewidth',1, 'color','r');hold on;plot(t{i},tuenv,'linewidth',1, 'color','k');hold on; plot(t{i}(devepoch),tuenv(devepoch),'k.','MarkerSize',6);axis([restrictTime(1) restrictTime(2)  -30 30]);
         text(2.25,10,['T' num2str(CRtrials(i))]);
     end
     set(gcf,'PaperPositionMode','auto');
