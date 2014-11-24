7d8a89cfc8dfd5cedb19f5c5aff5c1c1874037a5
gnattar <gnattar@gmail.com>
2014-11-24 01:42:26 -0500

    recalculate abs  kappa, plot with and without separation

--- main/Scripts_temp/checking_kappa.m
+++ main/Scripts_temp/checking_kappa.m
@@ -1,57 +1,74 @@
-n=1
+n=4
+a = []; ac = {};
+a(:,1) = pooled_contactCaTrials_thetadep_Silenced_SW{n}.lightstim;
+a(:,2)=pooled_contactCaTrials_thetadep_Silenced_SW{n}.poleloc;
+a(:,3)=pooled_contactCaTrials_thetadep_Silenced_SW{n}.totalKappa;
+a(:,4)=pooled_contactCaTrials_thetadep_Silenced_SW{n}.sigmag;
+a(:,5)=pooled_contactCaTrials_thetadep_Silenced_SW{n}.trialnum;
 
-a(:,1) = pooled_contactCaTrials_thetadep_Silenced{n}.lightstim;
-a(:,4)=pooled_contactCaTrials_thetadep_Silenced{n}.sigmag;
-a(:,3)=pooled_contactCaTrials_thetadep_Silenced{n}.totalKappa;
-a(:,2)=pooled_contactCaTrials_thetadep_Silenced{n}.poleloc;
-a(:,5)=pooled_contactCaTrials_thetadep_Silenced{n}.trialnum;
-
-ac(:,1)=pooled_contactCaTrials_thetadep_Silenced{n}.touchdeltaKappa;
-ac(:,2)=pooled_contactCaTrials_thetadep_Silenced{n}.contacts';
-ac(:,3)=pooled_contactCaTrials_thetadep_Silenced{n}.contactdir;
-ac(:,4)=pooled_contactCaTrials_thetadep_Silenced{n}.timews;
+ac(:,1)=pooled_contactCaTrials_thetadep_Silenced_SW{n}.touchdeltaKappa;
+ac(:,2)=pooled_contactCaTrials_thetadep_Silenced_SW{n}.contacts';
+ac(:,3)=pooled_contactCaTrials_thetadep_Silenced_SW{n}.contactdir;
+ac(:,4)=pooled_contactCaTrials_thetadep_Silenced_SW{n}.timews;
 ploc = unique(a(:,2));
 
 
-x=3
+x=2
 id = find((a(:,1)==0) & (a(:,2)==ploc(x)))
 stemp = a((a(:,1)==0) & (a(:,2)==ploc(x)),4);
 ktemp = a((a(:,1)==0) & (a(:,2)==ploc(x)),3);
 figure;plot(ktemp,stemp,'o');vline(0,'k--')
 title(['Prox Cell' num2str(n) ' poleloc' num2str(x) ' ' num2str(ploc(x)) ', ' num2str(length(stemp)) ' NL'])
+axis auto;
 
-id = find((a(:,1)==01) & (a(:,2)==ploc(x)))
+id = find((a(:,1)==01) & (a(:,2)==ploc(x)));
 stemp = a((a(:,1)==1) & (a(:,2)==ploc(x)),4);
 ktemp = a((a(:,1)==1) & (a(:,2)==ploc(x)),3);
-figure;plot(ktemp,stemp,'o');vline(0,'k--')
+hold on ; plot(ktemp,stemp,'ro');vline(0,'k--')
 title(['Prox Cell' num2str(n) ' poleloc' num2str(x) ' ' num2str(ploc(x)) ', ' num2str(length(stemp)) ' L'])
 
-axis([-35 35 0 5000]);
+axis auto;
 
 
 
 %%
 
-j=32
+% j=47
 figure;plot(ac{j,4}*500,ac{j,1}); title(num2str(a(j,5)));
-touchind =floor(ac{j,2}(:));
-temp = round(ac{j,4}*500);
+touchind =unique(ac{j,2}(:));
+temp = unique(round(ac{j,4}*500));
 [ri,ti,ci]= intersect(temp,touchind);
-hold on;  plot(ri,ac{j,1}(ti),'r*')
-figure;plot(pooled_contactCaTrials_thetadep_Silenced{n}.rawdata(j,:));hline(0,'k-')
-t = ac{j, 2};
+hold on;  plot(ri,ac{j,1}(ti),'r*');
+figure;plot(pooled_contactCaTrials_thetadep_Silenced_SW{n}.rawdata(j,:));hline(0,'k-');
+t = unique(ac{j, 2});
 ind=find(diff(t)~=1);
-count =1;Peakpercontact=0;
-for p = 1:length(ind)
-    vals = ac{j,1}(ti(count:ind(p))) ;
-    contdir = (median(vals) >0) *0 +(median(vals)< 0)*1;
-    if (contdir)
-        Peakpercontact =Peakpercontact + min(vals );
-    else
-        Peakpercontact =Peakpercontact + max(vals );
+count =0;Peakpercontact=0;Peakpercontact_abs=0;
+
+for p = 1:(length(ind)+1)
+    
+        if(p >length(ind))
+               vals = ac{j,1}(ti(count+1:end)) ;
+        else
+             vals = ac{j,1}(ti(count+1:ind(p))) ;
+        end
+ 
+    contdir = (abs(max(vals)) > abs(min(vals))) *0 +   (abs(max(vals)) < abs(min(vals)))  *1;
+    if (contdir &  min(vals )<.3)
+        Peakpercontact = Peakpercontact + min(vals );
+        Peakpercontact_abs = Peakpercontact_abs + abs(min(vals));
+    elseif(max(vals)>.3)
+        Peakpercontact = Peakpercontact + max(vals );
+        Peakpercontact_abs = Peakpercontact_abs + abs(max(vals));
     end
-count = count +ind(p);
+    if(p>length(ind))
+    else
+    count = ind(p);
+    end
+  
 end
+Peakpercontact
+Peakpercontact_abs
+smag = nansum(pooled_contactCaTrials_thetadep_Silenced_SW{n}.rawdata(j,:))
 %%
 
 
